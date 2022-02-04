94ac6c37f14c6f0d9f2868d2fe8996592bad249a
Gazsus <92299686+Gazsus@users.noreply.github.com>
2022-02-04 19:36:49 +0100

    Strings commit

---
+++ Stings/Reverse.py
@@ -0,0 +1,12 @@
+# Create a function called 'reverse()' which takes a string as a parameter
+# Pass the toBeReversed variable to this method to check if it works well
+
+
+toBeReversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+
+def reversed_string(string):
+    return string[::-1]
+
+
+print(reversed_string(toBeReversed))
---
+++ Stings/Takes longer.py
@@ -0,0 +1,13 @@
+# When saving this quote a disk error has occured. Please fix it!
+# Insert the words "always takes longer than" between the words "It" and "you"!
+
+quote_1 = "Hofstadter's Law: It "
+
+quote_2 = "you expect, even when you take into account Hofstadter's Law."
+
+quote_3 = "always takes longer than "
+
+
+quote = quote_1 + quote_3 + quote_2
+
+print(quote)
