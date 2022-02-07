ed4c6ffb811d5e5f92956fa9ab55c093bdb038cf
Gazsus <92299686+Gazsus@users.noreply.github.com>
2022-02-07 14:06:10 +0100

    Personal finance

---
+++ Data structures/Personal finance.py
@@ -0,0 +1,16 @@
+
+
+list = [500, 1000, 1250, 175, 800, 120]
+
+list_sum = sum(list)
+
+list_max = max(list)
+
+list_min = min(list)
+
+list_av = float('%.4f' % float(sum(list) / len(list)))
+
+print(list_sum)
+print(list_max)
+print(list_min)
+print(list_av)
